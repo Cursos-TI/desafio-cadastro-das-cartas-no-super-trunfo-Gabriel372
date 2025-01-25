46066d192cf80e7f52e9ac34c9fcf8e6a455aaeb
Gabriel Brandão <124525153+Gabriel372@users.noreply.github.com>
2025-01-25 21:07:45 +0000

    update

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+#include <string.h>
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
-    return 0;
+//declaracao das variaveis
+char codigo[20], nome[20];
+int populacao;
+float area,PIB;
+int numeroDePontosTuristicos ;
+//intercao com  o usuario
+printf("Digite o codigo da cidade:\n");
+scanf("%19s",codigo);
+    getchar();
+printf("Digite o nome da cidade:\n");
+fgets(nome, sizeof(nome), stdin);
+    nome[strcspn(nome, "\n")] = '\0';
+printf("Digite a quantidade da populacao:\n");
+scanf("%d",&populacao);
+printf("Digite a dimensao da area em km2:\n");
+scanf("%f",&area);
+printf("Digite o valor do PIB:\n");
+scanf("%f",&PIB);
+printf("Digite a quantidade de pontos turisticos:\n");
+scanf("%d",&numeroDePontosTuristicos);
+//resultado da interacao
+printf("==================CARTA CADASTRADA!=========\n");
+printf("Codigo da cidade:%s\n",codigo);
+printf("Nome da cidade:%s\n",nome);
+printf("Quantidade da populacao:%d\n",populacao);
+printf("Dimensao da area:%.1f km2\n",area);
+printf("PIB:%.1f R$\n",PIB);
+printf("Quantidade de pontos turisticos:%d\n",numeroDePontosTuristicos);
+return 0;
 }
